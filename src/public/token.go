70f0b02985098656b691d462cabe9e0a05bef970
Adie Olalekan <ugbeadie@gmail.com>
2020-05-27 18:54:17 +0100

    added public API endpoint

---
+++ src/public/token.go
@@ -0,0 +1,104 @@
+package public
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/AdieOlami/auth-service/src/errors"
+	"github.com/go-resty/resty/v2"
+)
+
+const (
+	headerXPublic   = "X-Public"
+	headerXClientId = "X-Client-Id"
+	headerXCallerId = "X-Caller-Id"
+
+	paramAccessToken = "access_token"
+)
+
+type accessToken struct {
+	Id       string `json:"id"`
+	UserId   string `json:"userId"`
+	ClientId string `json:"clientId"`
+}
+
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+func GetCallerId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	callerId, err := strconv.ParseInt(request.Header.Get(headerXCallerId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerId
+}
+
+func GetClientId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	clientId, err := strconv.ParseInt(request.Header.Get(headerXClientId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return clientId
+}
+
+func AuthenticateRequest(request *http.Request) *errors.Error {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenId := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenId == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenId)
+	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return nil
+		}
+		return err
+	}
+	request.Header.Add(headerXClientId, fmt.Sprintf("%v", at.ClientId))
+	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.UserId))
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+	request.Header.Del(headerXClientId)
+	request.Header.Del(headerXCallerId)
+}
+
+func getAccessToken(accessTokenId string) (*accessToken, *errors.Error) {
+
+	var at accessToken
+
+	client := resty.New()
+
+	client.SetHostURL("http://localhost:9091")
+
+	_, err := client.R().
+		SetResult(&at). // or SetResult(AuthSuccess{}).
+		Get(fmt.Sprintf("/auth/access_token/%s", accessTokenId))
+
+	if err != nil {
+		return nil, errors.NewInternalServerError(fmt.Sprintf("invalid restclient response when trying to login user %s", err.Error()))
+	}
+	return &at, nil
+}
